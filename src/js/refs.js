15f2738e494f68e32322ee2a1654e1a40d94fb7a
Andriy Romanov <mgm.agro04@gmail.com>
2025-03-29 17:43:49 +0200

    start coding

---
+++ src/js/refs.js
@@ -0,0 +1,15 @@
+/*export const refs = {
+  form: document.querySelector('#task-form'),
+  taskInput: document.querySelector('input[name="taskName"]'), // Поле вводу завдання
+  addTaskButton: document.querySelector('.header-form-btn'), // Кнопка додавання
+  taskList: document.querySelector('#task-list'), // Список завдань
+  themeToggleButton: document.querySelector('#themeToggle'), // Кнопка перемикання теми
+  taskDelete: document.querySelector('.task-list-item-btn'), //кнопка видалення карток
+  body: document.body, // Тег body, для зміни теми
+};*/
+export const refs = {
+  form: document.querySelector('#task-form'),
+  list: document.querySelector('#task-list'),
+  btnCT: document.querySelector('.theme-toggle-button'),
+  body: document.querySelector('body'),
+};
--- src/main.js
+++ src/main.js
@@ -14,3 +14,50 @@
       <p>Текст</p>
   </li>
 */
+import { refs } from './js/refs';
+import { createMarkup, createCard } from './js/markup-tasks';
+import { renderHTML } from './js/render-tasks';
+import { getStorageData, writeStorageData } from './js/local-storage-api';
+import { onBtnChangeTheme, initTheme } from './js/theme-switcher';
+
+let cards = getStorageData();
+
+initTheme();
+
+renderHTML(createMarkup(cards));
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.list.addEventListener('click', onBtnDelete);
+refs.btnCT.addEventListener('click', onBtnChangeTheme);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const name = refs.form.elements.taskName.value.trim();
+  const descr = refs.form.elements.taskDescription.value.trim();
+  const cardId = nanoid();
+
+  const card = {
+    name,
+    description: descr,
+    id: cardId,
+  };
+
+  cards.push(card);
+
+  writeStorageData(cards);
+
+  renderHTML(createCard(card));
+
+  refs.form.reset();
+}
+
+function onBtnDelete(evt) {
+  if (evt.target.nodeName === 'BUTTON') {
+    const id = evt.target.closest('[data-id]').dataset.id;
+    cards = cards.filter(card => id !== card.id);
+    writeStorageData(cards);
+    refs.list.innerHTML = '';
+    renderHTML(createMarkup(cards));
+  }
+}
